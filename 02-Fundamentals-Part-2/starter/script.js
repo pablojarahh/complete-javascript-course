1ef99a77c022267ca2e19fdb925b99f7771287d3
Pablo Jara <pablo.jarahh@gmail.com>
2024-05-18 00:52:31 -0400

    Agregado hasta challenge #1

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -17,9 +17,8 @@ console.log(appleJuice);
 
 const appleOrangeJuice = fruitProcessor(2, 4);
 console.log(appleOrangeJuice);
-*/
 
-// function declaration. Pueden llamarse antes de ejecutar la funcion
+// function declaration. #Leccion34 Pueden llamarse antes de ejecutar la funcion
 const age1 = calcAge1(1994);
 
 function calcAge1(birthYear) {
@@ -34,3 +33,53 @@ const calcAge2 = function (birthYear) {
 }
 const age2 = calcAge2(1994)
 console.log(age1, age2);
+
+// arrow functions #Leccion35
+const calcAge3 = birthYear => 2024 - birthYear;
+const age3 = calcAge3(1994);
+console.log(age3);
+
+const yearsUntilRetirement = (birthYear, firstName) => {
+    const age = 2024 - birthYear;
+    const retirement = 65 - age;
+    // return retirement
+    return `${firstName} retires in ${retirement} years`;
+}
+
+console.log(yearsUntilRetirement(1994, 'Pablo'));
+
+// Functions calling other Functions #Leccion36
+
+const cutPieces = function (fruit) {
+    return fruit * 4;
+}
+
+function fruitProcessor(apples, oranges) {
+    const applePieces = cutPieces(apples);
+    const orangePieces = cutPieces(oranges);
+    const juice = `Juice with ${applePieces} apples and ${orangePieces} oranges.`;
+    return juice;
+}
+
+console.log(fruitProcessor(2, 3));
+
+// #Challenge1
+
+// calculo del promedio
+const calcAverage = (score1, score2, score3) => (score1 + score2 + score3) / 3;  
+// almacenado del promedio de cada equipo
+const scoreDolphins = calcAverage(85,54,41);
+const scoreKoalas = calcAverage(23,34,27);
+// funcion para generar un ganador
+const checkWinner = function (avgDolphins, avgKoalas){
+    if(avgDolphins > avgKoalas){
+        return console.log(`Dolphins win (${avgDolphins} vs. ${avgKoalas})`)
+    } else if (avgKoalas > avgDolphins){
+        return console.log(`Koalas win (${avgKoalas} vs. ${avgDolphins})`)
+    } else {
+        return console.log(`No team wins...`)
+    }
+}
+
+checkWinner(scoreDolphins, scoreKoalas);
+*/
